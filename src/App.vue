d2b19c3e26d8e8dd71c459ada9ea3dadf3dd8bee
GW1402535\GW1402535 <shoo7830@gmail.com>
2017-08-31 16:41:16 +0900

    wp데이터 연동 파일

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <post-list></post-list>
+  </div>
+</template>
+
+<script>
+import PostList from './components/PostList';
+
+export default {
+  components: {
+    PostList
+  }
+}
+</script>
+
+<style lang='scss'>
+body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,code,form,fieldset,legend,textarea,p,blockquote,th,td,input,select,textarea,button {margin:0;padding:0}
+fieldset,img {border:0 none}
+dl,ul,ol,menu,li {list-style:none}
+blockquote, q {quotes: none}
+blockquote:before, blockquote:after,q:before, q:after {content:'';content:none}
+input,select,textarea,button {vertical-align:middle}
+input[type='text'],input[type='password']{-webkit-appearance:none;border-radius:0/*Safari/Chrome*/}
+button {border:0 none;background-color:transparent;cursor:pointer}
+body {background:#fff}
+body,th,td,input,select,textarea,button {font-size:18px;line-height:1.5;font-family:Apple Gothic,"Malgun Gothic",dotum,"Arial", sans-serif;color:#333}
+a {color:#333;text-decoration:none}
+a:active, a:hover {text-decoration:none}
+caption {visibility:hidden;overflow:hidden;width:1px;height:0;font-size:0;line-height:0;}
+address,cite,code,dfn,em,var {font-style:normal;font-weight:normal}
+table {border-collapse:collapse;border-spacing:0}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+  <ul v-if="postlists.length">
+    <li v-for="postlist in postlists">
+      <p v-for="url in postlist.attachments"><img :src="url.url"></p>
+      <p><a :href="postlist.url" target="_blank" rel="noopener">{{ postlist.title_plain }}</a></p>
+      <div v-html="postlist.content"  class="post-content"></div>
+      <p><a :href="postlist.url" target="_blank" rel="noopener">더보기</a></p>
+    </li>
+  </ul>
+  </div>
+</template>
+<script>
+// import axios from 'axios'
+import 'expose-loader?$!expose-loader?jQuery!jquery'
+export default {
+  name: 'post-list',
+  data: () => ({
+    postlists: []
+  }),
+
+  // data: function () {
+	// 	return {
+  //     postlists:[]
+  // },
+  
+  mounted: function () {
+    var self = this;
+    $.ajax({
+        url: 'http://webholic.net/api/get_recent_posts/?count=4',
+        method: 'GET',
+        success: function (response) {
+            self.postlists = response.posts;
+        },
+        error: function (error) {
+            alert(JSON.stringify(error));
+        }
+    });
+  }
+
+  // Fetches posts when the component is created.
+  // created() {
+  //   axios.get(`http://webholic.net/api/get_recent_posts/?count=4`)
+  //   .then(response => {
+  //     // JSON responses are automatically parsed.
+  //     this.postlists = response.data.posts
+  //   })
+  //   .catch(e => {
+  //     this.errors.push(e)
+  //   })
+    
+  // }
+
+    
+}
+
+
+
+  var postlists;
+
+  $.ajax({
+      url: 'http://webholic.net/api/get_recent_posts/?count=4',
+      method: 'GET',
+      success: function (response) {
+          this.postlists = response.posts;
+          //alert(JSON.stringify(data));
+          //console.log(postlists)
+      },
+      error: function (error) {
+          alert(JSON.stringify(error));
+      }
+  });
+</script>
+
+<style lang="scss">
+h1, h2, h3, h4, h5, h6 {font-size: 16px}
+h2 {
+  font-weight: normal;
+}
+ul {
+  &:after {
+    display: block;
+    content: '';
+    clear: both;
+  }
+
+  li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    float: left;
+    width: 25%;
+  }
+}
+
+.post {
+  margin: 0;
+  padding: 0;
+
+  h2 {
+    font-weight: normal;
+    //font-size: 14px;
+  }
+}
+
+.post-content {
+  height:208px;
+  overflow: hidden;
+}
+</style>
